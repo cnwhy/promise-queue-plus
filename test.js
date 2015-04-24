e7ffe814b7352d236cc56512d132d612e61d2bfb
why <w.why@163.com>
2015-04-25 00:09:50 +0800

    bugfix

--- test.js
+++ test.js
@@ -3,23 +3,23 @@ var alignment = require('./index').Promise;
 
 var alignment1 = new alignment(10,function(data){
 	//console.log(">>>>>>>>>>>>", arguments)
-	//console.log('第' + data + '个事件完成 - 正在运行的中的事件数：' + alignment1.ing + ' - 剩余运行队列：' + alignment1.lins.length)
+	console.log('第' + data + '个事件完成 - 正在运行的中的事件数：' + alignment1.ing + ' - 剩余运行队列：' + alignment1.lins.length)
 },function(err,obj){
-	console.log(arguments)
-	obj.errNumber < 3 && this.jump(obj)  // 运行次数小于3 则重试
+	console.log(err)
+	//obj.errNumber < 3 && this.jump(obj)  // 运行次数小于3 则重试
 },null,function(){
 	console.log("运行队列己空!",alignment1.ing)
-})
+},{retryON:3})
 
 function testfun(i){
 	var deferred = q.defer();
 	setTimeout(function(){
-		if(i%50 == 1){
+		if(i%3 == 1){
 			deferred.reject(new Error("err " + i))
 		}else{
 			deferred.resolve(i)
 		}
-	},(Math.random() * 2000)>>0)
+	},(Math.random() * 5000)>>0)
 	return deferred.promise;
 }
 // console.log('----------------push')
@@ -28,14 +28,14 @@ function testfun(i){
 // alignment1.go(testfun,[9])
 // return;
 
-var leng = 60;
+var leng = 10;
 for(var i = 0; i<leng; i++){
 	+function(){
 		var k = i;
 		alignment1.push(testfun,[k]).done(function(data){
-			console.log("__________________OK___________________",k)
+			//console.log("__________________OK___________________",k)
 		},function(err){
-			console.log("__________________Err___________________",k)
+			//console.log("__________________Err___________________",k)
 		})
 	}()
 }
@@ -49,9 +49,9 @@ setTimeout(function(){
 		return testfun(999)
 	})
 	.done(function(data){
-		console.log("__________________OK___________________")
+		console.log("__________________999OK___________________")
 	},function(){
-		console.log("__________________Err___________________")
+		console.log("__________________999Err___________________")
 	})
 },1000)
 
