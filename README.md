ed8f0c7a69880a73ffd2c2454158681466724d18
wanghangyang_pc <w.why@163.com>
2015-04-29 19:19:01 +0800

    re

--- README.md
+++ README.md
@@ -1,21 +1,86 @@
-lib-queue
-=====
+#lib-queue
+lib-queue 是一款轻量级 node异步方法 运行队列控制类。以均衡间歇性高并发的系统负载，提高node服务稳定性。
+暂时只实现Promise队列
 
-事件队列控制模块  
+###Promise队列
+#####lib-queue.Promise( q ) 
+初始化 参数**q** 
+- 为空 队列方法: `push` `unshift` `go` `jump` 无返回值;
+- true 插入队列方法: `push` `unshift` `go` `jump` 返回Promise ,可执行独立回调
+- 也可传入q模块, 插入方法反回功能更强大的Promise的
+#####实例化队列 new lib-queue.Promise()(runMax,con) 
+- runMax 并行运行队列方法的最大个数
+-  con 配置队列 **开始 结束** 事件,运行单元的 **成功,失败** 事件及配置执行单元出错的 **重试** 机制。  
 
-Promise模式
-var Queue = require('lib-queue').Promise(1);
-var queue = new Queue.Promise(maxleng)
+实例化队列 
+```
+var queue = new lib-queue.Promise(true)(100,{
+		"event_succ":function(){}  //成功
+		,"event_err":function(){}  //失败
+		,"event_begin":function(){}  //队列开始
+		,"event_end":function(){}    //队列完成
+		,"event_add":function(){}    //有执行项添加进执行单元后执行,注意 go 及 jump方法不会触发
+		,"retryON":0				 //单元出错重试次数
+		,"retryType":0               //重试模式  0:搁置执行(插入队列尾部重试),1:优先执行 (插入队列头部重试)
+	})`
+```
+####API
+##### queue.push( promisefun , args[] , con)
+向队列中尾部添加运行单元
+fun: promise function
+args: 传入的参数
+con:  配置
+```javascript
+{
+	'event_succ':null
+	,'event_err':null
+	,'Queue_event':true //默认会执行队列定义的回调
+}
+```
+#####queue.unshift( fun , args[] , con) 同push 向队列中头部添加运行单元
+#####queue.go( fun , args[] , con)  同push,添加后启动队列.
+#####queue.jump( fun , args[] , con) 同unshift,添加后启动队列.
+#####setMax(newMax)
+修改并行运行队列方法的最大个数
+#####queue.start() 
+启动队列
+#####queue.stop()
+暂停队列 
+#####queue.clear() 
+清空队列
 
-//队列控制
-.push(fun,args,con) //向尾部添加执行单元  
-.go(fun,args,con) //向尾部添加执行单元 并执行 
-.unshift(fun,args,con) //向头部添加执行单元  
-.jump(fun,args,con) //向头部添加执行单元 并优先执行 
-
-.start()  //开始执行
-.clear()  //清除执行队列  
-.setMax(max,noplay)  动态修改最大并行数
-
-
-
+### demo
+``` javascript
+var libq = require('lib-queue');  //引入
+//初始化Promise异步队列类,参数1表示带Promise类型的反回
+var Queue = libq.Promise(1); 
+var queue1 = new Queue(2,{
+	"event_succ":function(data){console.log('queue-succ:',data)}  //成功
+	,"event_err":function(err){console.log('queue-succ:',data)}  //失败
+}); //实列化最大并发为2的运行队列
+var q = libq.Q;  //模块中简单实现了Q的基本功能，可以一试，
+//定义一个Promise风格的异步方法
+function testfun(i){
+	var deferred = q.defer();
+	setTimeout(function(){
+		if(i && i % 3 == 0){
+			deferred.reject(new Error("err " + i))
+		}else{
+			deferred.resolve(i)
+		}
+	},(Math.random() * 2000)>>0)
+	return deferred.promise;
+}
+//向队列添加运行单元
+queue1.push(testfun,[1]) //添加运行项
+queue1.go(testfun,[2]) //添加并自动启动队列
+queue1.go(testfun,[3],{Queue_event:0}) //添加不会触发队列 回调的运行项.
+queue1.go(testfun,[4]).done(
+	function(data){console.log('done-succ:',data)},
+	function(err){console.log('done-err:',err)}
+)
+queue1.go(testfun,[5],{
+	event_succ:function(data){console.log('conf-succ:',data)},
+	event_err:function(err){console.log('conf-err:',err)}
+})
+```
--- test_q&q.js
+++ test_q&q.js
@@ -1,70 +1,102 @@
-//var q = require("q");
-var q = require('./index').Q;
+var q = require("q");
+var q_ = require('./index').Q;
 var Q0 = require('./index').Promise();
 var Q1 = require('./index').Promise(1);
 var Q2 = require('./index').Promise(q);
-function testfun(i){
+
+function testfun(i,err){
 	var deferred = q.defer();
 	setTimeout(function(){
-		if(i%3 == 1 || i == "Q3"){
+		if(err){
 			deferred.reject(new Error("err " + i))
 		}else{
 			deferred.resolve(i)
 		}
-	},(Math.random() * 4000)>>0)
+	},(Math.random() * 2000)>>0)
+	return deferred.promise;
+}
+
+function testfun_(i,err){
+	var deferred = q_.defer();
+	setTimeout(function(){
+		if(err){
+			deferred.reject(i)
+		}else{
+			deferred.resolve(i)
+		}
+	},(Math.random() * 2000)>>0)
 	return deferred.promise;
 }
 
 function QandQ(){
-	console.log('run')
+	console.log('>>>>添加效率测试')
 	q0 = new Q0(1000)
 	q1 = new Q1(1000)
 	q2 = new Q2(1000)
 	
 	var maxl = 100000
-	var d0 = new Date()
-	for(var k=0;i<maxl;k++){
-		q1.push(testfun,[k])
+	var d0 = new Date();
+	for(var k=0;k<maxl;k++){
+		q0.push(testfun,[k],{
+			"event_succ":function(data){}  //成功
+			,"event_err":function(err){}  //失败
+		})
 	}
-	var d1 = new Date()
+	var d1 = new Date();
+	console.log("0: ",d1 - d0)
 	for(var i=0;i<maxl;i++){
 		q1.push(testfun,[i]).done(function(){},function(){})
 	}
-	var d2 = new Date()
+	var d2 = new Date();
+	console.log("1: ",d2 - d1)
 	for(var n=0;n<maxl;n++){
 		q2.push(testfun,[n]).done(function(){},function(){})
 	}
 	var d3 = new Date();
-	console.log("0: ",d1 - d0)
-	console.log("1: ",d2 - d1)
 	console.log("q: ",d3 - d2)
-	console.log('end')
+	console.log('<<<<添加效率检测完毕')
 }
 
-function textQ(){
-	// var kk = testfun(1).then(function(d){
-		// console.log(d)
-	// });
-	
-	// for(var i in kk){
-		// console.log(i , kk[i])
-	// }
-	//console.log(kk)
-	
-	testfun(1).then(function(data){
-		console.log(data);
-		var k = testfun(2)
-		//console.log(k)
-		return k;
-	})
-	.then(function(data){
-		console.log(data);
-		return testfun(3)
-	}).done(function(data){
-		console.log(data);
-	},function(err){
-		console.log(err);
+function Qtest(){
+	console.log('>>>> Qtest');
+	_test().then(function(data){
+		console.log(data)
+		if('> done - succ OK' == data){
+			console.log('> then OK')
+		}
+		_test(1).fail(function(err){
+			console.log(err)
+			if('> done - err OK' == err){console.log('> fail ok')}
+			console.log('<<<< Qtest end')
+		})
 	})
 }
-textQ()
-//QandQ();
+
+function _test(a){
+	var deferred = q_.defer();
+	testfun_(1,0).then(function(data){
+		console.log(data)
+		return testfun_(2,a || 0)
+	}).then(function(data){
+		console.log(data)
+		return testfun_(3,0)
+	}).done(function(data){
+		console.log(data);
+		if(!a && data == 3){
+			deferred.resolve('> done - succ OK');
+		}else{
+			deferred.resolve('> done - succ Err');
+		}
+	},function(err){
+		console.log(err + " -- err")
+		if(a && err == 2){
+			deferred.reject('> done - err OK')
+		}else{
+			deferred.reject('> done - err Err')
+		}
+	})
+	return deferred.promise;
+}
+QandQ();  //插入效率测试
+Qtest();  //正常工作测试
+
