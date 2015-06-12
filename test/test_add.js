8c7a7064dbb0e278236e038048e4e75223c95496
why <w.why@163.com>
2015-06-12 10:59:20 +0800

    增加效率测试

--- test/test_add.js
+++ test/test_add.js
@@ -6,7 +6,31 @@ var Q0 = QueueFun.Queue(Promise);
 var Q1 = QueueFun.Queue(q);
 var Q2 = QueueFun.Queue();
 
-function testfun(i,err){
+function getTestFun(Pro){
+	return function(i,err){
+		var deferred = Pro.defer()
+		setTimeout(function(){
+			if(err){
+				deferred.reject(err)
+			}else{
+				deferred.resolve(i)
+			}
+		},50)
+		return deferred.promise;
+	}
+}
+function testfun0(i,err){
+	var deferred = Promise.defer()
+	setTimeout(function(){
+		if(err){
+			deferred.reject(err)
+		}else{
+			deferred.resolve(i)
+		}
+	},(Math.random() * 200)>>0)
+	return deferred.promise;
+}
+function testfun1(i,err){
 	var deferred = q.defer();
 	setTimeout(function(){
 		if(err){
@@ -17,8 +41,7 @@ function testfun(i,err){
 	},(Math.random() * 200)>>0)
 	return deferred.promise;
 }
-
-function testfun_(i,err){
+function testfun2(i,err){
 	var deferred = q_.defer();
 	setTimeout(function(){
 		if(err){
@@ -29,42 +52,88 @@ function testfun_(i,err){
 	},(Math.random() * 200)>>0)
 	return deferred.promise;
 }
+var maxl = 50000,bxs = 500;
+var q0 = new Q0(bxs)
+var q1 = new Q1(bxs)
+var q2 = new Q2(bxs)
+var _fun = function(){
 
-function test_add(){
-	var deferred = q_.defer();
-	try{
-		console.log('>>>>添加效率测试')
-		var q0 = new Q0(1000)
-		var q1 = new Q1(1000)
-		var q2 = new Q2(1000)
-		
-		var maxl = 50000
-		var d0 = new Date();
-		for(var k=0;k<maxl;k++){
-			q0.push(testfun,[k]).then(console.log,console.error)
-		}
-		var d1 = new Date();
-		console.log(d1 - d0," >> 原生Promise ES6")
-		for(var i=0;i<maxl;i++){
-			q1.push(testfun,[i]).then(console.log,console.error)
-		}
-		var d2 = new Date();
-		console.log(d2 - d1," >> q模块")
-		for(var n=0;n<maxl;n++){
-			q2.push(testfun,[n]).then(console.log,console.error)
-		}
-		var d3 = new Date();
-		console.log(d3 - d2," >> 内置Promise")
-		console.log('<<<<添加效率检测完毕')
-		deferred.resolve("OK")
-	}catch(e){
-		deferred.reject(e);
+}
+function test0(){
+	console.log('>>>>> 原生Promise ES6 测试开始')
+	var def = Promise.defer();
+	var testfun = getTestFun(Promise)
+	var d0 = new Date();
+	for(var k=0;k<maxl;k++){
+		q0.push(testfun,[k]).then(_fun,_fun)
 	}
-	return deferred.promise;
+	var d1 = new Date();
+	console.log("添加 " + maxl + " 元素耗时  ---- " + (d1 - d0))
+	q0.option("event_end",function(){
+		var d2 = new Date();
+		console.log("" + bxs + " 并行完成队列耗时  ---- " + (d2 - d1))
+		console.log('<<<<<<<<<<<<<<')
+		def.resolve()
+	})
+	q0.start();
+	return def.promise;
 }
-if(module && module.parent){
-	module.exports = test_add;
-}else{
-	test_add();
+function test1(){
+	console.log('>>>>> q模块 测试开始')
+	var def = q.defer();
+	var testfun = getTestFun(q)
+	var d0 = new Date();
+	for(var k=0;k<maxl;k++){
+		q1.push(testfun,[k]).then(_fun,_fun)
+	}
+	var d1 = new Date();
+	console.log("添加 " + maxl + " 元素耗时  ---- " + (d1 - d0))
+	q1.option("event_end",function(){
+		var d2 = new Date();
+		console.log("" + bxs + " 并行完成队列耗时  ---- " + (d2 - d1))
+		console.log('<<<<<<<<<<<<<<')
+		def.resolve()
+	})
+	q1.start();
+	return def.promise;
 }
+function test2(){
+	console.log('>>>>> 内置Promise 测试开始')
+	var def = q_.defer();
+	var testfun = getTestFun(q_)
+	var d0 = new Date();
+	for(var k=0;k<maxl;k++){
+		q2.push(testfun,[k]).then(_fun,_fun)
+	}
+	var d1 = new Date();
+	console.log("添加 " + maxl + " 元素耗时  ---- " + (d1 - d0))
+	q2.option("event_end",function(){
+		var d2 = new Date();
+		console.log("" + bxs + " 并行完成队列耗时  ---- " + (d2 - d1))
+		console.log('<<<<<<<<<<<<<<')
+		def.resolve()
+	})
+	q2.start();
+	return def.promise;
+}
+//test0().then(test1).then(test2)
+
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("开始测试? ", function(answer) {
+	rl.close();
+	test0().then(test1).then(test2)
+});
+//test1();
+//test2();
+
+// if(module && module.parent){
+// 	module.exports = test_add;
+// }else{
+// 	test_add();
+// }
 
